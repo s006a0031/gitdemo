420bc7708760f7fa65d0a1e62b1074e93aa1a7a7
s006a0031 <2571093998@qq.com>
2020-03-23 20:41:20 +0800

    initial update

---
+++ fileinfo.py
@@ -0,0 +1,8 @@
+#Script Name         : 7-2.py
+#Author              : Wanghaoyu
+#Created             : 21th May 2019
+#Last Modified       :
+#Version             : 1.0
+#Modifications       :
+
+#Description         : 
